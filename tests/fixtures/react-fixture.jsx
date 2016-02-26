782d91b8a98b527a56df6e351d0c2100ef8e4da0
Ivan Montiel <idmontie@asu.edu>
2016-02-26 10:25:32 -0700

    Add tests and linters

---
+++ tests/fixtures/react-fixture.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default ({ name, description }) => (
+  <div className="container">
+    <div className="name">{name}</div>
+    <div className="description">{description}</div>
+  </div>
+);
